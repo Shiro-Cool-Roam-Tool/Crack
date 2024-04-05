1f4f06e619d6f198b0bd8b7d4706b5ceb5b5a2b0
Shiro-Cool-Roam-Tool <162789398+Shiro-Cool-Roam-Tool@users.noreply.github.com>
2024-04-05 07:37:19 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os,platform
 os.system('git pull')
 os.system("clear")
-crk=platform.architecture()[0]
-if crk=="32bit":
+crack=platform.architecture()[0]
+if crack=="32bit":
     __import__("file32")
-elif crk=="64bit":
+elif crack=="64bit":
     __import__("file64")
